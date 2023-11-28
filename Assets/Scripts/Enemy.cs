7846ab8b8b1c12aad34d35bfc9afa7b14154f933
Doğukan Gözüm <dogukangozum@hotmail.com>
2023-11-28 18:20:16 +0300

    enemy created

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+    [SerializeField] float radius;
+
     private void Update()
     {
         
 
     }
 
-    private void Attack()
+    [ContextMenu("Run")]
+    private Unit Attack()
     {
-        
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius);
+        float closestPos = 100;
+        Unit closestUnit = null;
+        foreach (Collider2D c in enemies)
+        {
+            float newClosest = Vector2.Distance(transform.position, c.transform.position);         
+
+            if (newClosest<closestPos)
+            {
+                closestPos = newClosest;
+                closestUnit = c.GetComponent<Unit>();
+                Debug.Log(closestUnit.name);
+            }
+
+
+        }
+
+        return closestUnit;
     }
 
 
